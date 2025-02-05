955e122ac0f1bb01901246b782f8bd9c4c20e896
logendsjeje1651 <lebrunt70@gmail.com>
2025-02-05 05:38:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        prefixe: '-', // Préfixe - Le texte avant les commandes
+        buyer: '1150451712931401780', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'ingencylive', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
